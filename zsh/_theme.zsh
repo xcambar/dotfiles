def07c3aa9b0930ea1394ab8ace0fec30e99671b
Xavier Cambar <xcambar@gmail.com>
2018-08-16 12:02:33 +0200

    puts purs in PATH

--- zsh/_theme.zsh
+++ zsh/_theme.zsh
@@ -1,5 +1,5 @@
 function zle-line-init zle-keymap-select {
-  PROMPT=`/Users/xav/code/projects/purs/target/release/purs prompt -k "$KEYMAP" -r "$?"`
+  PROMPT=`purs prompt -k "$KEYMAP" -r "$?"`
   zle reset-prompt
 }
 zle -N zle-line-init
@@ -8,6 +8,6 @@ zle -N zle-keymap-select
 autoload -Uz add-zsh-hook
 
 function _prompt_purs_precmd() {
-  /Users/xav/code/projects/purs/target/release/purs precmd
+  purs precmd
 }
 add-zsh-hook precmd _prompt_purs_precmd
